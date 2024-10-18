5b5a57bf6f33b78fabaf03a9bc1c4ee05ead2083
Divya Premanantha <premananthadivya@gmail.com>
2024-10-18 16:57:20 +0530

    Change config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.adminAPI = "http://host.docker.internal:8080/admin/";
-config.apiMetaDataAPI = "http://host.docker.internal:9090/apiMetadata/"
+config.adminAPI = "https://82d26742-afe1-4ed2-bf5e-0034635f0a07-dev.e1-us-east-azure.choreoapis.dev/eci-devportal/devportalbackend/api-metadata-endpoint/v1.0/admin/";
+config.apiMetaDataAPI = "https://82d26742-afe1-4ed2-bf5e-0034635f0a07-dev.e1-us-east-azure.choreoapis.dev/eci-devportal/api-metadata-service/v1.0/apiMetadata/"
 config.env = 'local'
 config.apiImageURL = "http://localhost:9090/apiMetadata/"
 
