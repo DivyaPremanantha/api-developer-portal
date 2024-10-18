5f4600b7a0e4b9ead61df681e9994bc65b0e92bb
Divya Premanantha <premananthadivya@gmail.com>
2024-10-18 23:51:21 +0530

    Fix admin and api URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.adminAPI = "https://82d26742-afe1-4ed2-bf5e-0034635f0a07-dev.e1-us-east-azure.choreoapis.dev/eci-devportal/devportalbackend/api-metadata-endpoint/v1.0/admin/";
-config.apiMetaDataAPI = "https://82d26742-afe1-4ed2-bf5e-0034635f0a07-dev.e1-us-east-azure.choreoapis.dev/eci-devportal/api-metadata-service/v1.0/apiMetadata/"
+config.adminAPI = "https://82d26742-afe1-4ed2-bf5e-0034635f0a07-dev.e1-us-east-azure.choreoapis.dev/eci-devportal/devportalbackend/api-metadata-endpoint/v1.0/";
+config.apiMetaDataAPI = "https://82d26742-afe1-4ed2-bf5e-0034635f0a07-dev.e1-us-east-azure.choreoapis.dev/eci-devportal/api-metadata-service/v1.0/"
 config.env = 'local'
 config.apiImageURL = "http://localhost:9090/apiMetadata/"
 
